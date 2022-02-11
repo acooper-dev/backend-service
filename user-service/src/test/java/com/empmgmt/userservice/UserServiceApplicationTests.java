d23d3533222a6dc570b32189a431e1a0caafc1c0
mohitrawat48 <mohitrawat48@gmail.com>
2022-02-11 17:15:29 -0500

    add user-service

---
+++ user-service/src/test/java/com/empmgmt/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empmgmt.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
